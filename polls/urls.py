e6369412ea15ca92b421b99449d44380b4de6b7a
Abhiram1214 <abhiram_cvns@hotmail.com>
2017-11-20 08:19:43 +0200

    Updated questions and choices
    Updated Questions and choices for each question

--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^users/', views.users, name='users'),
+    url(r'^users/$', views.users, name='users'),
+    url(r'^questions/$', views.questions, name='questions'),
+    url(r'^(?P<question_id>[0-9]+)/$', views.choices, name='choices'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from polls.forms import NewUserForm
+from polls.models import Question, Choice
 # Create your views here.
 
 def index(request):
     return render(request,'polls/index.html')
+
+def questions(request):
+    questions_list = Question.objects.all()
+    return render(request,'polls/questions.html',{'question_list':questions_list})
+
+def choices(request, question_id):
+    question = Question.objects.get(id=question_id)
+    choices = question.choice_set.all()
+    return render(request, 'polls/choices.html',{'choices':choices})
+
 
 def users(request):
     form = NewUserForm()
